caac2daf8bebccd0d8191f344f7fdc54c9195383
Xiaoyiawa <1079802404@qq.com>
2026-09-17 20:30:12 +0800

    恢复本地浅色主题的 custom_msgbox（仅更新头文件路径为 utils/）

--- views/custom_msgbox.c
+++ views/custom_msgbox.c
@@ -1,8 +1,8 @@
 #include "custom_msgbox.h"
 
 #include "main.h"
-#include "page_manager.h"
-#include "lv_utils.h"
+#include "utils/page_manager.h"
+#include "utils/lv_utils.h"
 
 static void custom_msgbox_delete_cb(lv_event_t * e) {
     page_back();
@@ -10,6 +10,7 @@ static void custom_msgbox_delete_cb(lv_event_t * e) {
 
 lv_obj_t * custom_msgbox_create(const char *title, const char *txt, const char **btn_txts, bool add_close_btn)
 {
+
     lv_obj_t * mbox_bg = lv_obj_create(lv_scr_act());
     lv_obj_remove_style_all(mbox_bg);
     lv_obj_clear_flag(mbox_bg, LV_OBJ_FLAG_SCROLLABLE);
@@ -18,8 +19,6 @@ lv_obj_t * custom_msgbox_create(const char *title, const char *txt, const char *
     lv_obj_set_style_bg_color(mbox_bg, lv_color_black(), LV_STATE_DEFAULT);
     lv_obj_t * mbox = lv_msgbox_create(mbox_bg, title, txt,
                 btn_txts, add_close_btn);
-    lv_obj_set_style_max_height(mbox, lv_pct(100), LV_STATE_DEFAULT);
-    lv_obj_set_size(mbox, lv_pct(100), LV_SIZE_CONTENT);
     lv_obj_center(mbox);
     lv_obj_add_event_cb(mbox, custom_msgbox_delete_cb, LV_EVENT_DELETE, NULL);
 
@@ -37,7 +36,7 @@ static void custom_toast_timer_cb(lv_timer_t * t) {
 
 lv_obj_t * custom_toast_create(const char *txt)
 {
-    lv_obj_t * mbox = lv_label_create(lv_layer_top());
+    lv_obj_t * mbox = lv_label_create(lv_scr_act());
     lv_label_set_long_mode(mbox, LV_LABEL_LONG_WRAP);
     lv_label_set_text(mbox, txt);
     lv_obj_set_style_bg_opa(mbox, LV_OPA_60, LV_STATE_DEFAULT);
